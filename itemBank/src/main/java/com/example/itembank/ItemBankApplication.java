b1674556510d9dc609088aecdad1efb9bb5f7cd6
dongyue.yu <oakyue@outlook.com>
2022-08-19 22:18:54 +0800

    刷题之旅

---
+++ itemBank/src/main/java/com/example/itembank/ItemBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.itembank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemBankApplication.class, args);
+    }
+
+}
